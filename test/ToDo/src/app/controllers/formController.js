1e7eb239ae4c22330ea25afee5a0112a3f075dda
ksajinishanilka <sajinikoongahawattage@outlook.com>
2018-02-09 20:22:04 +0530

    removing accidentally added folder

--- test/ToDo/src/app/controllers/formController.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Sajini on 08-Feb-18.
- */
-var app = angular.module('ToDo', []);
-app.controller('formController', function($scope) {
-  $scope.task = "John";
-  $scope.date = "Doe";
-  $scope.description = "Doe";
-});
